a38f6b4ccb72df56770fbc175d0eeb4ad22dc3d0
migdalius <30595371+migdalius@users.noreply.github.com>
2023-02-13 07:43:30 +0100

    start template

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+const Container = styled.div``;
+const Home = () => {
+  return (
+    <Container>
+      <div>start</div>
+    </Container>
+  );
+};
+
+export default Home;
